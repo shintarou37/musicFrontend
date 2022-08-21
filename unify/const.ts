546cb54c8c4e7cd9ab474ef2aa1cd6609e20c32e
shintarou37 <m.sintarou37@gmail.com>
2022-08-21 22:11:08 +0900

    [Launch] 雛形作成

---
+++ unify/const.ts
@@ -0,0 +1,11 @@
+export const apiURL: string = 'http://localhost:8080';
+
+export interface Obj {
+  "ID": number,
+  "CreatedAt": string,
+  "UpdatedAt": string,
+  "DeletedAt": string,
+  "name": string,
+  "reason": string,
+  "artist": string,
+}
