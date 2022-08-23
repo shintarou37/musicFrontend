0bd9f4a8cc8344060372c1bc265c78faaf6e3a86
shintarou37 <m.sintarou37@gmail.com>
2022-08-23 10:58:42 +0900

    [Launch] 一覧画面のサーバー側の実装

--- unify/const.ts
+++ unify/const.ts
@@ -6,12 +6,19 @@ export const axiosBase = axios.create({
   baseURL: 'http://localhost:8080',
 });
 
-export interface Obj {
+export interface MusicObj {
+  "ID": number,
+  "Name": string,
+  "Artist": string,
+  "Reason": string,
+  "Mst_situationName": string,
+}
+
+export interface SituationObj {
   "ID": number,
   "CreatedAt": string,
   "UpdatedAt": string,
-  "DeletedAt": string,
+  "DeletedAt": null,
   "Name": string,
-  "Artist": string,
-  "Reason": string
+  "Musics": null
 }
