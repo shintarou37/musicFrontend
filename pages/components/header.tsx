cc8a41b0398139b7235ae45f090fdea0490bcf1b
shintarou37 <m.sintarou37@gmail.com>
2022-08-25 22:09:44 +0900

    [Launch] ヘッダー作成

---
+++ pages/components/header.tsx
@@ -0,0 +1,9 @@
+import styles from '../../styles/Home.module.css'
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <a href="/" className={styles.logo}>オンレコ</a>
+    </header>
+  )
+}
