19691512f477711c67422e5b869d0e3a5b37b35d
shintarou37 <m.sintarou37@gmail.com>
2022-08-25 22:37:52 +0900

    [LAunch] 投稿ボタンを表示

--- pages/components/header.tsx
+++ pages/components/header.tsx
@@ -4,6 +4,7 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <a href="/" className={styles.logo}>オンレコ</a>
+      <a className={`${styles.logo} ${styles.new}`}>投稿する</a>
     </header>
   )
 }
