afe113cbf0f103bc23905719a8a7d1285bf1d97e
shintarou37 <m.sintarou37@gmail.com>
2022-08-29 08:34:29 +0900

    [Launch] refactoring

---
+++ pages/components/list.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+
+import styles from '../../styles/Home.module.css'
+import { MusicObj } from '../../unify/const'
+
+export default function List(music: MusicObj[]) {
+    const musics = music.map((value: MusicObj, key: number) => {
+        let detalPath = `/detal/?id=${value.ID}`;
+        // keyはユニークIDとして付与している
+        return <div key={key} className={styles.list}>
+            <p>シチュエーション</p>
+            <span className={styles.listName}>{value.Mst_situationName}</span>
+            <p>曲名</p>
+            <span className={styles.listName}>{value.Name}</span>
+            <p>歌手名</p>
+            <span className={styles.listName}>{value.Artist}</span>
+            <p>おすすめポイント</p>
+            <span className={styles.listName}>{value.Reason}</span><br></br>
+            <Link href={detalPath}>
+                <a>詳細</a>
+            </Link>
+        </div>
+    })
+    return musics
+}
