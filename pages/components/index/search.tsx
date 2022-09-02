548e983b6be0c9a0033954ddb87b1662fc4541db
shintarou37 <m.sintarou37@gmail.com>
2022-09-02 11:33:34 +0900

    [Fix] build error解消

--- pages/components/index/search.tsx
+++ pages/components/index/search.tsx
@@ -2,12 +2,14 @@ import styles from '../../../styles/Home.module.css'
 import { SituationObj, searchArg } from '../../../unify/obj'
 
 export default function Search(props: searchArg) {
-    const search = props.Mst_situation.map((value: SituationObj, key: number)=> {
-        return <button className={`${styles.item} ${props.search == value.ID ? styles.select : ""}`} key={key} value={value.ID} onClick={(e) => { console.log(e.target.value), props.setSearch(e.target.value)}}>{value.Name}</button>
-    })
+  const search = props.Mst_situation.map((value: SituationObj, key: number) => {
+    return <button className={`${styles.item} ${Number(props.search) == value.ID ? styles.select : ""}`} key={key} value={value.ID} onClick={(e) => {
+      props.setSearch(e.currentTarget.value)
+    }}>{value.Name}</button>
+  })
   return (
     <div className={styles.contents}>
-        {search}
+      {search}
     </div>
   )
 }
