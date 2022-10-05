b0d288f4ea01524632b6a636957652ad161ce4a0
shintarou37 <m.sintarou37@gmail.com>
2022-10-05 09:58:51 +0900

    [Update] HOME/LISTのコンポーネントのテスト概要を追加する

--- __tests__/test.spec.tsx
+++ __tests__/test.spec.tsx
@@ -1,11 +1,33 @@
+import List from '../components/index/list'
+import { listArg, MusicObj } from '../unify/obj'
 import Header from '../components/header'
+import Home from '../pages/index'
 import { render, screen } from "@testing-library/react";
+
+const music: any = [{Artist: "1", ID: 1, Mst_situationName: "1つ目です", Name: "名前です", Reason: "理由です"}]
 
 describe("Header", () => {
   it("表示されること", () => {
     render(<Header />);
 
-    // テキストを利用してテスト対象を抽出する方法
     expect(screen.getByText("オンレコ")).toBeTruthy();
+  });
+});
+
+describe("Home", () => {
+  it("表示されること", () => {
+    render(<Home/>);
+
+    expect(screen.getByText("投稿する")).toBeTruthy();
+  });
+});
+
+describe("LIST", () => {
+  it("表示されること", () => {
+    render(<List music={music}/>);
+
+    expect(screen.getByText("1つ目です")).toBeTruthy();
+    expect(screen.getByText("名前です")).toBeTruthy();
+    expect(screen.getByText("理由です")).toBeTruthy();
   });
 });
