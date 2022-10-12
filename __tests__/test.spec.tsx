74732ffd0cc5c7b8ee8de291f5320e653940fefb
shintarou37 <m.sintarou37@gmail.com>
2022-10-12 10:19:12 +0900

    [Test] テストコードの大枠を作成

--- __tests__/test.spec.tsx
+++ __tests__/test.spec.tsx
@@ -1,6 +1,7 @@
 import List from '../components/index/list'
+import DetailList from '../components/details/list'
 import Register from '../components/index/register'
-import { listArg, MusicObj } from '../unify/obj'
+import { listArg, MusicObj, detailListArg } from '../unify/obj'
 import Header from '../components/header'
 import Home from '../pages/index'
 import { render, screen } from "@testing-library/react";
@@ -8,12 +9,17 @@ import { render, screen } from "@testing-library/react";
 const music: any = [{Artist: "1", ID: 1, Mst_situationName: "1つ目です", Name: "名前です", Reason: "理由です"}]
 const situations: any = [{ID: 1,  Name: "名前です"}]
 const setFunction = (()=>{})
+const MusicObj: MusicObj = {Artist: "1", ID: 1, Mst_situationName: "1つ目です", Name: "名前です", Reason: "理由です"};
+
 
 describe("Header", () => {
   it("Header 表示されること", () => {
     render(<Header />);
+    // screen.debug();
 
     expect(screen.getByText("オンレコ")).toBeTruthy();
+    // aタグ
+    expect(screen.getByRole("link")).toBeTruthy();
   });
 });
 
@@ -21,7 +27,12 @@ describe("Home", () => {
   it("Home 表示されること", () => {
     render(<Home/>);
 
+    expect(screen.getByText("オンレコ")).toBeTruthy();
     expect(screen.getByText("投稿フォーム")).toBeTruthy();
+
+    expect(screen.getByRole("link")).toBeTruthy();
+    expect(screen.getByRole("button")).toBeTruthy();
+
   });
 });
 
@@ -36,6 +47,9 @@ describe("LIST", () => {
     expect(screen.getByText("曲名")).toBeTruthy();
     expect(screen.getByText("歌手名")).toBeTruthy();
     expect(screen.getByText("おすすめポイント")).toBeTruthy();
+    expect(screen.getByText("詳細")).toBeTruthy();
+
+    expect(screen.getByRole("link")).toBeTruthy();
   });
 });
 
@@ -49,5 +63,30 @@ describe("Register", () => {
     expect(screen.getByText("歌手名（1 ~ 100文字）")).toBeTruthy();
     expect(screen.getByText("おすすめポイント（1 ~ 1000文字）")).toBeTruthy();
     expect(screen.getByText("投稿する")).toBeTruthy();
+
+    expect(screen.getAllByRole("textbox")[0]).toBeTruthy();
+    expect(screen.getAllByRole("textbox")[1]).toBeTruthy();
+    expect(screen.getAllByRole("textbox")[2]).toBeTruthy();
+    // selectタグ
+    expect(screen.getByRole("combobox")).toBeTruthy();
+    expect(screen.getByRole("button")).toBeTruthy();
+
+  });
+});
+
+describe("DetailList", () => {
+  it("DetailList 表示されること", () => {
+    render(<DetailList data={MusicObj} createdAt={"2000/01/01 11:11"}/>);
+    
+    expect(screen.getByText("1つ目です")).toBeTruthy();
+    expect(screen.getByText("名前です")).toBeTruthy();
+    expect(screen.getByText("理由です")).toBeTruthy();
+    expect(screen.getByText("シチュエーション")).toBeTruthy();
+    expect(screen.getByText("曲名")).toBeTruthy();
+    expect(screen.getByText("歌手名")).toBeTruthy();
+    expect(screen.getByText("おすすめポイント")).toBeTruthy();
+
+    expect(screen.getByRole("heading")).toBeTruthy();
+    expect(screen.getByRole("link")).toBeTruthy();
   });
 });
