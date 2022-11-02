3aebfe229d6304ef27d26f44d0aaa3cac296546d
shintarou37 <m.sintarou37@gmail.com>
2022-11-02 10:13:20 +0900

    [Test] 編集フォームのテストコードを追加

--- __tests__/components.spec.tsx
+++ __tests__/components.spec.tsx
@@ -6,6 +6,7 @@ import Header from '../components/header'
 import IndexList from '../components/index/list'
 import IndexRegister from '../components/index/register'
 import DetailsList from '../components/details/list'
+import DetailsEdit from '../components/details/edit'
 import UsersSignUp from '../components/users/signupForm'
 import UsersSignIn from '../components/users/signinForm'
 
@@ -60,6 +61,33 @@ describe("indexディレクトリ", () => {
 describe("detailsディレクトリ", () => {
   it("DetailsList 表示されること", () => {
     render(<DetailsList data={MusicObj} createdAt={"2000/01/01 11:11"} updatedAt={"3000/12/30 00:00"}/>);
+    expect(screen.getByText("詳細画面")).toBeTruthy();
+
+    expect(screen.getByText("シチュエーション")).toBeTruthy();
+    expect(screen.getByText("1つ目です")).toBeTruthy();
+
+    expect(screen.getByText("曲名")).toBeTruthy();
+    expect(screen.getByText("名前です")).toBeTruthy();
+
+    expect(screen.getByText("歌手名")).toBeTruthy();
+    expect(screen.getByText("1")).toBeTruthy();
+
+    expect(screen.getByText("おすすめポイント")).toBeTruthy();
+    expect(screen.getByText("理由です")).toBeTruthy();
+
+    expect(screen.getByText("投稿者")).toBeTruthy();
+    expect(screen.getByText("UserName")).toBeTruthy();
+    expect(screen.getByText("投稿日")).toBeTruthy();
+    expect(screen.getByText("2000/01/01 11:11")).toBeTruthy();
+    expect(screen.getByText("更新日")).toBeTruthy();
+    expect(screen.getByText("3000/12/30 00:00")).toBeTruthy();
+
+    expect(screen.getByText("トップ画面へ戻る")).toBeTruthy();
+
+    expect(screen.getByRole("heading")).toBeTruthy();
+  });
+  it("DetailsEdit 表示されること", () => {
+    render(<DetailsEdit data={MusicObj} createdAt={"2000/01/01 11:11"} updatedAt={"3000/12/30 00:00"}/>);
     screen.debug();
 
     expect(screen.getByText("詳細画面")).toBeTruthy();
